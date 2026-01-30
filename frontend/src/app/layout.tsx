aeb8d29a9845650477270dabcc1d7cb6451e2a4b
tuy250 <dieudonnetuy250@gmail.com>
2026-01-30 10:36:49 +0200

    init

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -160,6 +160,8 @@ export default function RootLayout({
           async 
           data-cfasync="false">
         </script>
+        {/* Muzikax.com Popunder Ad Tag */}
+        <script src="https://pl28605916.effectivegatecpm.com/a9/c3/dc/a9c3dc3a3a8377732b2d96aa69ab8c62.js"></script>
       </head>
       <body className="pb-24 md:pb-0">
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com"}>
